e562f70223e24803d61978750f6232eba7e7c163
Chris Schubert <chris.a.schubert25@gmail.com>
2021-05-22 13:31:49 -0400

    Updating logos

--- scripts/svgo/config.js
+++ scripts/svgo/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     multipass: false, // boolean. false by default
-    datauri: 'base64', // 'base64', 'enc' or 'unenc'. 'base64' by default
+    //datauri: 'base64', // 'base64', 'enc' or 'unenc'. 'base64' by default
     floatPrecision: 2,
     js2svg: {
       indent: 2, // string with spaces or number of spaces. 4 by default
